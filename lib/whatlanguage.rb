18cdffe3398fa9fecb0238ac7fcda3dbbce2d699
Jiří Hradil <jiri.hradil@kyberie.cz>
2014-12-14 12:32:49 +0100

    remove puts

--- lib/whatlanguage.rb
+++ lib/whatlanguage.rb
@@ -3,7 +3,7 @@ require 'whatlanguage/bitfield'
 require 'digest/sha1'
 
 class WhatLanguage
-  HASHER = lambda { |item| puts item; Digest::SHA1.digest(item.downcase.strip).unpack("VV") }
+  HASHER = lambda { |item| Digest::SHA1.digest(item.downcase.strip).unpack("VV") }
 
   BITFIELD_WIDTH = 2_000_000
 
